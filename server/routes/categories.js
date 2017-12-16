0e74c5231e5995c335cd5c942e80515b2c88f64d
wahibhanii <wahib.hanii@gmail.com>
2017-12-16 23:14:11 +0700

    complete CRUD categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const CategoriesController = require('../controllers/categories-controller');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Find one category
+router.get('/:id', CategoriesController.findOneCategory)
+
+// Create new category
+router.post('/', CategoriesController.createCategory);
+
+// Delete category
+router.delete('/:id', CategoriesController.deleteCategory);
+
+// Add new task to the category
+router.put('/:id/addlist', CategoriesController.addList);
+
+// Remove task from the category
+router.put('/:id/removelist', CategoriesController.removeList);
+
+// Rename category
+router.put('/:id/rename', CategoriesController.renameCategory);
+
 
 module.exports = router;
